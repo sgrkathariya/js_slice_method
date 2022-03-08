3d333ab091853ff93e88f92da8d5fa257d33133b
sgrkathariya <sgrkathariya@gmail.com>
2022-03-09 01:29:39 +0545

    first commit

---
+++ map.js
@@ -0,0 +1,8 @@
+// the map() method creates a new array with the results of calling a function for every array elements
+
+
+const arr=[1,4,9,16];
+const mapping =arr.map(x=>x*2);
+console.log(mapping);
+
+// output: [ 2, 8, 18, 32 ]
