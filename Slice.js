0d3374985a8a2125d400c6f8aab73473c97b29d3
sgrkathariya <sgrkathariya@gmail.com>
2022-03-09 01:10:36 +0545

    First Commit

---
+++ Slice.js
@@ -0,0 +1,10 @@
+
+
+
+// the slice() method selects a part of an array and returns the new array
+
+const arr=['Toyota','BMW','Mahindra','TATA','Honda','Maruti Suzuki'];
+const arrSlice= arr.slice(1,4);
+console.log(arrSlice);
+
+//output : [ 'BMW', 'Mahindra', 'TATA' ]
